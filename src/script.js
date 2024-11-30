965a6b08fb1b75df3751ad83a0ed57f0452a74e8
Sergio <sergio.ruedagonzalez@riberadeltajo.es>
2024-11-30 18:00:13 +0100

    Avances en la página index del proyecto

---
+++ src/script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('imageContainer');
+    const images = Array.from(container.getElementsByTagName('img'));
+    const totalImages = images.length;
+  
+    // Establecer el intervalo de cambio de imágenes
+    setInterval(() => {
+      const firstImage = images[0];  // Imagen más a la izquierda
+      const lastImage = images[images.length - 1];  // Imagen más a la derecha
+  
+      // Mover la primera imagen a la última posición
+      container.appendChild(firstImage);
+  
+      // Actualizar el array de imágenes
+      images.push(firstImage);
+      images.shift(); // Eliminar la imagen más a la izquierda del array
+  
+    }, 4000); // Intervalo de 2 segundos entre cada movimiento
+  });
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,31 @@ module.exports = {
         el tipo de fuente utilizando fontFamily y probablemente mas cosas igual */
         fondo: "pink",
         texto: "green",
-      }
+        'olivine': {
+        '50': '#f5f9ec',
+        '100': '#e8f2d5',
+        '200': '#d3e5b1',
+        '300': '#b6d482',
+        '400': '#a1c567',
+        '500': '#7ca53d',
+        '600': '#5f832d',
+        '700': '#4a6526',
+        '800': '#3d5123',
+        '900': '#354621',
+        '950': '#1a250e',
+    },
+      },
+      animation: {
+        fade: 'fadeInOut 10s infinite',
+      },
+      keyframes: {
+        fadeInOut: {
+          'from': { opacity: '1' },
+          'to': { opacity: '0' },
+        },
+      },
+    
+      
     },
   },
   plugins: [],
