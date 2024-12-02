0b581d93f30ef9c7a3728d8dd2217d57f102a1b5
Sergio <sergio.ruedagonzalez@riberadeltajo.es>
2024-12-02 11:36:58 +0100

    Nav desplegalble de pagina inicio

--- src/script.js
+++ src/script.js
@@ -1,22 +1,24 @@
-
-
+//cambios de imagenes en personalizacion cnc
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.getElementById('imageContainer');
     const images = Array.from(container.getElementsByTagName('img'));
     const totalImages = images.length;
-  
-    // Establecer el intervalo de cambio de imágenes
+
     setInterval(() => {
-      const firstImage = images[0];  // Imagen más a la izquierda
-      const lastImage = images[images.length - 1];  // Imagen más a la derecha
-  
-      // Mover la primera imagen a la última posición
+      const firstImage = images[0]; 
+      const lastImage = images[images.length - 1];
       container.appendChild(firstImage);
-  
-      // Actualizar el array de imágenes
       images.push(firstImage);
-      images.shift(); // Eliminar la imagen más a la izquierda del array
-  
-    }, 4000); // Intervalo de 2 segundos entre cada movimiento
+      images.shift();  
+    }, 4000); // Intervalo de 2 segundos
   });
+
+//evento del vertical nav burguer
+const verticalNav = document.getElementById('verticalNav');
+const navburger = document.getElementById('navburger');
+
+const mostrarMenu=()=>{
+  verticalNav.classList.toggle('hidden');
+}
+navburger.addEventListener('click',mostrarMenu);
   
